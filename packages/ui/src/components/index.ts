4abf0c9646d8260c183b0df5843372583e8ca50f
Aileen Villanueva <aileenvl@gmail.com>
2025-07-16 10:10:21 -0600

    update theme and name

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -8,4 +8,4 @@ export { SearchResults } from "./SearchResults";
 export { SearchRoot } from "./SearchRoot";
 export { Suggestions } from "./Suggestions";
 export { SlidingPanel } from "./SlidingPanel";
-export { default as Icon } from "./Icon";
+export { default as OramaLogo } from "./OramaLogo";
