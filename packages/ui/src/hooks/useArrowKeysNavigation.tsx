8dc9d45ef7c2e2e215571e748ab2ab9822cbb391
Francesca Giannino <francesca.giannino86@gmail.com>
2025-10-14 11:37:40 +0200

    fix: remove comment

--- packages/ui/src/hooks/useArrowKeysNavigation.tsx
+++ packages/ui/src/hooks/useArrowKeysNavigation.tsx
@@ -1,9 +1,9 @@
-import { useRef, useCallback } from "react";
+import { useRef, useCallback } from 'react'
 
 interface UseArrowKeysNavigationOptions {
-  selectorLeftRight?: string;
-  selector?: string;
-  disabled?: boolean;
+  selectorLeftRight?: string
+  selector?: string
+  disabled?: boolean
 }
 
 /**
@@ -37,108 +37,107 @@ interface UseArrowKeysNavigationOptions {
  * ```
  */
 export function useArrowKeysNavigation(
-  options: UseArrowKeysNavigationOptions = {},
+  options: UseArrowKeysNavigationOptions = {}
 ) {
   const {
-    selector = "[data-focus-on-arrow-nav]",
-    selectorLeftRight = "[data-focus-on-arrow-nav-left-right]",
-    disabled = false,
-  } = options;
-  const ref = useRef<HTMLElement>(null);
+    selector = '[data-focus-on-arrow-nav]',
+    selectorLeftRight = '[data-focus-on-arrow-nav-left-right]',
+    disabled = false
+  } = options
+  const ref = useRef<HTMLElement>(null)
 
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
-      if (disabled || !ref.current) return;
-      if (event.key !== "ArrowDown" && event.key !== "ArrowUp") return;
+      if (disabled || !ref.current) return
+      if (event.key !== 'ArrowDown' && event.key !== 'ArrowUp') return
 
-      event.stopPropagation();
-      event.preventDefault();
+      event.stopPropagation()
+      event.preventDefault()
 
-      const focusableElements = ref.current.querySelectorAll(selector);
-      // exclude elements with tabIndex="-1" or data-focus-on-arrow-nav="false" or aria-hidden="true"
+      const focusableElements = ref.current.querySelectorAll(selector)
       const focusableArray = Array.from(focusableElements).filter((element) => {
         const dataValue = (element as HTMLElement).getAttribute(
-          "data-focus-on-arrow-nav",
-        );
-        const tabIndex = (element as HTMLElement).getAttribute("tabIndex");
-        const ariaHidden = (element as HTMLElement).getAttribute("aria-hidden");
+          'data-focus-on-arrow-nav'
+        )
+        const tabIndex = (element as HTMLElement).getAttribute('tabIndex')
+        const ariaHidden = (element as HTMLElement).getAttribute('aria-hidden')
         return (
-          dataValue !== "false" && tabIndex !== "-1" && ariaHidden !== "true"
-        );
-      }) as HTMLElement[];
+          dataValue !== 'false' && tabIndex !== '-1' && ariaHidden !== 'true'
+        )
+      }) as HTMLElement[]
 
-      if (focusableArray.length === 0) return;
+      if (focusableArray.length === 0) return
 
-      const firstFocusableElement = focusableArray[0];
-      const lastFocusableElement = focusableArray[focusableArray.length - 1];
-      const focusedElement = ref.current.querySelector(":focus") as HTMLElement;
-      const focusedIndex = focusableArray.indexOf(focusedElement);
+      const firstFocusableElement = focusableArray[0]
+      const lastFocusableElement = focusableArray[focusableArray.length - 1]
+      const focusedElement = ref.current.querySelector(':focus') as HTMLElement
+      const focusedIndex = focusableArray.indexOf(focusedElement)
 
-      let nextFocusableElement: HTMLElement | undefined;
+      let nextFocusableElement: HTMLElement | undefined
 
-      if (event.key === "ArrowDown") {
+      if (event.key === 'ArrowDown') {
         nextFocusableElement =
           focusedIndex === focusableArray.length - 1
             ? firstFocusableElement
-            : focusableArray[focusedIndex + 1];
+            : focusableArray[focusedIndex + 1]
       } else {
         nextFocusableElement =
           focusedIndex === 0
             ? lastFocusableElement
-            : focusableArray[focusedIndex - 1];
+            : focusableArray[focusedIndex - 1]
       }
 
       if (nextFocusableElement) {
-        nextFocusableElement.focus();
+        nextFocusableElement.focus()
       }
     },
-    [selector, disabled],
-  );
+    [selector, disabled]
+  )
 
   const handleArrowLeftRight = useCallback(
     (event: KeyboardEvent) => {
-      if (disabled || !ref.current) return;
-      if (event.key !== "ArrowLeft" && event.key !== "ArrowRight") return;
+      if (disabled || !ref.current) return
+      if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') return
 
-      event.stopPropagation();
-      event.preventDefault();
-      const focusableElements = ref.current.querySelectorAll(selectorLeftRight);
+      event.stopPropagation()
+      event.preventDefault()
+      const focusableElements = ref.current.querySelectorAll(selectorLeftRight)
 
       const focusableArray = Array.from(focusableElements).filter((element) => {
-        const dataValue = element.getAttribute("data-focus-on-arrow-nav");
-        const tabIndex = element.getAttribute("tabIndex");
-        const ariaHidden = element.getAttribute("aria-hidden");
+        const dataValue = element.getAttribute('data-focus-on-arrow-nav')
+        const tabIndex = element.getAttribute('tabIndex')
+        const ariaHidden = element.getAttribute('aria-hidden')
         return (
-          dataValue !== "false" && tabIndex !== "-1" && ariaHidden !== "true"
-        );
-      }) as HTMLElement[];
+          dataValue !== 'false' && tabIndex !== '-1' && ariaHidden !== 'true'
+        )
+      }) as HTMLElement[]
 
-      if (focusableArray.length === 0) return;
+      if (focusableArray.length === 0) return
 
-      const focusedElement = ref.current.querySelector(":focus") as HTMLElement;
-      const focusedIndex = focusableArray.indexOf(focusedElement);
-      let nextFocusableElement: HTMLElement | undefined;
-      if (event.key === "ArrowRight") {
+      const focusedElement = ref.current.querySelector(':focus') as HTMLElement
+      const focusedIndex = focusableArray.indexOf(focusedElement)
+      let nextFocusableElement: HTMLElement | undefined
+      if (event.key === 'ArrowRight') {
         nextFocusableElement =
           focusedIndex === focusableArray.length - 1
             ? focusableArray[0]
-            : focusableArray[focusedIndex + 1];
+            : focusableArray[focusedIndex + 1]
       } else {
         nextFocusableElement =
           focusedIndex === 0
             ? focusableArray[focusableArray.length - 1]
-            : focusableArray[focusedIndex - 1];
+            : focusableArray[focusedIndex - 1]
       }
       if (nextFocusableElement) {
-        nextFocusableElement.focus();
+        nextFocusableElement.focus()
       }
     },
-    [selectorLeftRight, disabled],
-  );
+    [selectorLeftRight, disabled]
+  )
 
   return {
     ref,
     onKeyDown: handleKeyDown,
-    onArrowLeftRight: handleArrowLeftRight,
-  };
+    onArrowLeftRight: handleArrowLeftRight
+  }
 }
